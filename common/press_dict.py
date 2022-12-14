ae88392e41917c566447b328c153a1bc1532d563
Leicq130821 <583870243@qq.com>
2022-12-14 23:12:44 +0800

    提交base操作

---
+++ common/press_dict.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/12/14 23:10
+# @Author  : LCQ
+# @FileName: press_dict.py
+
+class PressDict():
+    # 拨号键
+    call=5
+    # 挂机键
+    end_call=6
+    # home键
+    home=3
+
+    KEYCODE_MENU
+    菜单键
+    82
+    KEYCODE_BACK
+    返回键
+    4
+    KEYCODE_SEARCH
+    搜索键
+    84
+    KEYCODE_CAMERA
+    拍照键
+    27
+    KEYCODE_FOCUS
+    拍照对焦键
+    80
+    KEYCODE_POWER
+    电源键
+    26
+    KEYCODE_NOTIFICATION
+    通知键
+    83
+    KEYCODE_MUTE
+    话筒静音键
+    91
+    KEYCODE_VOLUME_MUTE
+    扬声器静音键
+    164
+    KEYCODE_VOLUME_UP
+    音量增加键
+    24
+    KEYCODE_VOLUME_DOWN
+    音量减小键
+    25
