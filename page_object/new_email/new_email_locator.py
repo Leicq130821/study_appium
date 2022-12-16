8c54e18c8fcab38eee5ffda8be2dc8ed1a7d173f
Leicq130821 <583870243@qq.com>
2022-12-16 22:38:33 +0800

    更新log模块

--- page_object/new_email/new_email_locator.py
+++ page_object/new_email/new_email_locator.py
@@ -16,6 +16,6 @@ class NewEmailLocator(PublicLocator):
     # 客户联系人搜索编辑框
     customer_contact_search_edit_text=PublicLocator.follow_text_edit_text.format('客户联系人')
     # 添加的收件人
-    added_recipient='//*[@text="收件人 :"]/following-sibling::*//android.widget.TextView'
+    added_recipient='//android.widget.TextView[@text="收件人 :"]/following-sibling::*//android.widget.TextView'
     # 邮件正文编辑框
     email_content_edit_text='//*[@resource-id="cke_ckeditor"]//android.widget.EditText'
